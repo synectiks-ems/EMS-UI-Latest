b676293f0202e87b9058ff614edd8bfa955b201e
Jasmin Makasana <jasminmakasana1991@gmail.com>
2020-08-07 17:19:46 +0530

    Fix: "Application not enabled" error fixed.

--- public/app/features/plugins/plugin_page_ctrl.ts
+++ public/app/features/plugins/plugin_page_ctrl.ts
@@ -35,12 +35,12 @@ export class AppPageCtrl {
     this.page = _.find(app.includes, { slug: this.$routeParams.slug });
 
     if (!this.page) {
-      this.$rootScope.appEvent(AppEvents.alertError, ['App Page Not Found']);
+      // this.$rootScope.appEvent(AppEvents.alertError, ['App Page Not Found']);
       this.navModel = this.navModelSrv.getNotFoundNav();
       return;
     }
     if (app.type !== 'app' || !app.enabled) {
-      this.$rootScope.appEvent(AppEvents.alertError, ['Application Not Enabled']);
+      // this.$rootScope.appEvent(AppEvents.alertError, ['Application Not Enabled']);
       this.navModel = this.navModelSrv.getNotFoundNav();
       return;
     }
